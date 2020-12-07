f923f336bdd067d3ec94ea92fa88c09debdb2879
sbvhev <rohannepal200@gmail.com>
2020-12-06 17:16:43 -0800

    setup repo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cover-core-v2
+
+## Development
+* run `npm install` to install all node dependencies
+* run `npx hardhat compile` to compile
+
+### Run Test With hardhat EVM (as [an independent node](https://hardhat.dev/hardhat-evm/#connecting-to-hardhat-evm-from-wallets-and-other-software))
+* Run `npx hardhat node` to setup a local blockchain emulator in one terminal.
+* `npx hardhat test --network localhost` run tests in a new terminal.
+ **`npx hardhat node` restart required after full test run.** As the blockchain timestamp has changed.
---
+++ hardhat.config.js
@@ -0,0 +1,70 @@
+require("@nomiclabs/hardhat-waffle");
+// require("@nomiclabs/buidler-web3"); // necessary for time to work
+require('hardhat-contract-sizer');
+require('@nomiclabs/hardhat-etherscan');
+
+require('dotenv').config();
+
+// This is a sample Buidler task. To learn how to create your own go to
+// https://buidler.dev/guides/create-task.html
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.getAddress());
+  }
+});
+
+// You have to export an object to set up your config
+// This object can have the following optional entries:
+// defaultNetwork, networks, solc, and paths.
+// Go to https://buidler.dev/config/ to learn more
+module.exports = {
+  contractSizer: {
+    alphaSort: true,
+    runOnCompile: true,
+    disambiguatePaths: false,
+  },
+  optimizer: {
+    enabled: true,
+    runs: 200,
+  },
+  // defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+      loggingEnabled: true,
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545", // same address and port for both Buidler and Ganache node
+      gas: 8000000,
+      gasLimit: 8000000,
+      gasPrice: 1,
+    },
+    kovan: {
+      url: process.env.KOVAN_INFURA_URL ? process.env.KOVAN_INFURA_URL : '',
+      accounts: process.env.KOVAN_DEV_PRIVATE_KEY ? [`0x${process.env.KOVAN_DEV_PRIVATE_KEY}`] : [],
+      gas: 8000000,
+      gasLimit: 8000000,
+    },
+    mainnet: {
+      url: process.env.MAINNET_INFURA_URL ? process.env.MAINNET_INFURA_URL : '',
+      accounts: process.env.MAINNET_DEV_PRIVATE_KEY ? [`0x${process.env.MAINNET_DEV_PRIVATE_KEY}`] : [],
+      // gas: 8000000,
+      // gasLimit: 8000000,
+      // gasPrice: 65000000000,
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  // This is a sample solc configuration that specifies which version of solc to use
+  solidity: {
+    version: "0.7.3",
+    settings: {
+      optimizer: {
+        enabled: true
+      }
+    }
+  },
+};
